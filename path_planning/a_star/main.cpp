fd68c58b23242554e37bde5a4904b900c2d9e4fc
liuyongsheng <lysxx717@gmail.com>
2021-12-07 14:53:23 +0800

    format code

--- path_planning/a_star/main.cpp
+++ path_planning/a_star/main.cpp
@@ -5,19 +5,17 @@
 using namespace cpp_robotics::path_planning;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    //! load map data
-    cv::Mat map_data = cv::imread("../maps/map1.png", CV_8UC1);
-    if (map_data.empty()) {
-        std::cerr << "load map image fail." << std::endl;
-        return -1;
-    }
-
-    //! a_star
-    a_star::Astar astar(a_star::Heuristic::manhattan, true);
-    astar.init(map_data);
-    auto path = astar.findPath({25, 75}, {80, 10});
-
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  //! load map data
+  cv::Mat map_data = cv::imread("../maps/map2.png", CV_8UC1);
+  if (map_data.empty()) {
+    std::cerr << "load map image fail." << std::endl;
+    return -1;
+  }
+  //! a_star
+  std::cout << map_data.size() << std::endl;
+  a_star::Astar astar(a_star::Heuristic::euclidean, true);
+  astar.init(map_data);
+  auto path = astar.findPath({25,25}, {480, 480});
+  return 0;
 }
